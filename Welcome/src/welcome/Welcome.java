6e2f3a6b4398e1f2cfb83b5e0652945060fae66b
McSandvich <62926409+Aki-daichi@users.noreply.github.com>
2024-02-08 20:37:29 +0700

    Real files

---
+++ Welcome/src/welcome/Welcome.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package welcome;
+
+/**
+ *
+ * @author dzaka
+ */
+public class Welcome
+{
+   public static void main(String[] args)
+   {
+      String greeting = "Welcome to Core Java!";
+      System.out.println(greeting);
+      for (int i = 0; i < greeting.length(); i++)
+         System.out.print("=");
+      System.out.println();
+   }
+}
+
+/*
+ Output dari program ini adalah: 
+
+ Welcome to Core Java!
+=====================
+ */
